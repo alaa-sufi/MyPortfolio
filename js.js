a4b839c602be77bc46154c7121c7aa10d17422ff
Alaa <alaasufi227@gmal.com.com>
2021-01-18 23:06:21 +0200

    last edit

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+$(document).ready(function () {
+  "use strict";
 //smooth scroll and active 
 $(".mynav li").click(function(){
 $(this).addClass("active").siblings().removeClass("active");
@@ -5,25 +7,64 @@ $("html , body").animate({
   scrollTop:$("#" + $(this).data("scroll")).offset().top + 1
 },1000) 
 })
-// active in scroll
+
+
 $(window).scroll(function(){
+// active in scroll
   $(".about ,.skills ,.portfolio ,.contact").each(function(){
     var blockId= $(this).attr("id");
     if($(window).scrollTop()>$(this).offset().top){
        $(".mynav li").removeClass("active");
        $(".mynav li[data-scroll="+ blockId +"]").addClass("active");
-       //if  window.scrollY ==window.scrollMaxY- active contact
+//if  window.scrollY ==window.scrollMaxY- active contact
        if(window.scrollY==window.scrollMaxY){
         $(".mynav li").removeClass("active");
        $(".mynav li[data-scroll=contact]").addClass("active");
        }
+      //  solve scroll to about
+      
      }
    })
-})
+   
+//small nav in scroll
+//    if($(document).scrollTop() >= 50){
+//     $(".mynav").css({
+//       padding: 0,
+//       // opacity: 0.8
+//     }
+//     );
+    
+//     $(".mynav .logo").css("font-size","1.5rem");
+//   }
+//   else{
+//     $(".mynav").css({
+//       padding: "0.5rem 1rem",
+//       // opacity: 1
+//     }
+//     );
+    
+//     $(".mynav .logo").css({ 
+//     fontSize:"2rem"
+//     }
+//     );
+//   }
+ })
+
+// margin top for body to be nav best
+// $("body").css("margin-top",$(".mynav").height())
+
 
 //project link in about
 $(".about .main p span").click(function(){
   $("html , body").animate({
     scrollTop:$("#projects").offset().top + 1
   },1000) 
+})
+})
+
+// collps navigator in click 
+$(".nav-item").click(function(){
+$(".navbar-toggler").toggleClass("collapsed");
+$("#navbarSupportedContent").removeClass("show");
+  // $("#navbarSupportedContent").css("display","none")
 })

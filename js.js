226546d70698822712237f4f67171582e1eec13c
alaa-sufi <alaasufi227@gmail.com>
2022-04-03 01:02:05 +0200

    project json

--- js.js
+++ js.js
@@ -1,71 +1,105 @@
 $(document).ready(function () {
   "use strict";
-//smooth scroll and active 
-$(".mynav li").click(function(){
-$(this).addClass("active").siblings().removeClass("active");
-$("html , body").animate({
-  scrollTop:$("#" + $(this).data("scroll")).offset().top + 1
-},1000) 
-})
+  //smooth scroll and active 
+  $(".mynav li").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+    $("html , body").animate({
+      scrollTop: $("#" + $(this).data("scroll")).offset().top + 1
+    }, 1000)
+  })
 
 
-$(window).scroll(function(){
-// active in scroll
-  $(".about ,.skills ,.portfolio ,.contact").each(function(){
-    var blockId= $(this).attr("id");
-    if($(window).scrollTop()>$(this).offset().top){
-       $(".mynav li").removeClass("active");
-       $(".mynav li[data-scroll="+ blockId +"]").addClass("active");
-//if  window.scrollY ==window.scrollMaxY- active contact
-       if(window.scrollY==window.scrollMaxY){
+  $(window).scroll(function () {
+    // active in scroll
+    $(".about ,.skills ,.portfolio ,.contact").each(function () {
+      var blockId = $(this).attr("id");
+      if ($(window).scrollTop() > $(this).offset().top) {
         $(".mynav li").removeClass("active");
-       $(".mynav li[data-scroll=contact]").addClass("active");
-       }
-      //  solve scroll to about
-      
-     }
-   })
-   
-//small nav in scroll
-//    if($(document).scrollTop() >= 50){
-//     $(".mynav").css({
-//       padding: 0,
-//       // opacity: 0.8
-//     }
-//     );
-    
-//     $(".mynav .logo").css("font-size","1.5rem");
-//   }
-//   else{
-//     $(".mynav").css({
-//       padding: "0.5rem 1rem",
-//       // opacity: 1
-//     }
-//     );
-    
-//     $(".mynav .logo").css({ 
-//     fontSize:"2rem"
-//     }
-//     );
-//   }
- })
+        $(".mynav li[data-scroll=" + blockId + "]").addClass("active");
+        //if  window.scrollY ==window.scrollMaxY- active contact
+        if (window.scrollY == window.scrollMaxY) {
+          $(".mynav li").removeClass("active");
+          $(".mynav li[data-scroll=contact]").addClass("active");
+        }
+        //  solve scroll to about
 
-// margin top for body to be nav best
-// $("body").css("margin-top",$(".mynav").height())
+      }
+    })
+
+    //small nav in scroll
+    //    if($(document).scrollTop() >= 50){
+    //     $(".mynav").css({
+    //       padding: 0,
+    //       // opacity: 0.8
+    //     }
+    //     );
+
+    //     $(".mynav .logo").css("font-size","1.5rem");
+    //   }
+    //   else{
+    //     $(".mynav").css({
+    //       padding: "0.5rem 1rem",
+    //       // opacity: 1
+    //     }
+    //     );
+
+    //     $(".mynav .logo").css({ 
+    //     fontSize:"2rem"
+    //     }
+    //     );
+    //   }
+  })
+
+  // margin top for body to be nav best
+  // $("body").css("margin-top",$(".mynav").height())
 
 
-//project link in about
-$(".about .main p span").click(function(){
-  $("html , body").animate({
-    scrollTop:$("#projects").offset().top + 1
-  },1000) 
-})
+  //project link in about
+  $(".about .main p span").click(function () {
+    $("html , body").animate({
+      scrollTop: $("#projects").offset().top + 1
+    }, 1000)
+  })
 })
 
 // collps navigator in click 
-$(".nav-item").click(function(){
-$(".navbar-toggler").toggleClass("collapsed");
-$("#navbarSupportedContent").removeClass("show");
+$(".nav-item").click(function () {
+  $(".navbar-toggler").toggleClass("collapsed");
+  $("#navbarSupportedContent").removeClass("show");
   // $("#navbarSupportedContent").css("display","none")
 })
 var myFullpage = new fullpage("#fullpage", {});
+
+var projects;
+fetch('https://raw.githubusercontent.com/alaa-sufi/MyPortfolio/main/data.json')
+  .then(response => response.json())
+  .then(data => {
+    projects = data.projects;
+    console.log(projects)
+  
+    for(i=0;i<data.projects.length ;i++){
+      $("#projects .carousel").append(
+          `<div class="carousel-cell">
+        <div class="card one" style="width: 18rem">  
+         <img class="card-img-top" src=${projects[i].image} />
+            <div class="card-body">
+              <h5 class="card-title">${projects[i].title}</h5>
+              <div class="use">
+                <i class="icon fab fa-html5"></i>
+                <i class="icon fab fa-sass"></i>
+                <i class="icon fab fa-bootstrap"></i>
+                <img class="icon" src="img/jquery.png" alt="jquery" style="width: 1rem; height: 1rem" />
+                <h6 class="icon ticon">svg</h6>
+              </div>
+              <p class="card-text">
+               ${projects[i].desc}
+              </p>
+              <a class="get btn" href=${projects[i].url} target="blank"><i
+                class="fab fa-github"></i></a>
+              <a class="demo btn" href=${projects[i].gitHub}" target="blank"><i
+                class="fa fa-external-link-alt"></i></a>
+        </div>
+      </div>`)
+      }
+  });
+
